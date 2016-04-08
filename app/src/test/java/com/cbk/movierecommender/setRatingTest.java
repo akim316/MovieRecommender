00e0ebc42e99e22339c15b8c2c9f0240eed80249
Sana Ajani <sanaajani@msn.com>
2016-04-08 12:01:17 -0400

    junit

--- app/src/test/java/com/cbk/movierecommender/setRatingTest.java
+++ app/src/test/java/com/cbk/movierecommender/setRatingTest.java
@@ -7,20 +7,21 @@ package com.cbk.movierecommender;
 import org.junit.Test;
 
 import com.cbk.TechTrollywood.R;
+import android.test.InstrumentationTestCase;
 import com.firebase.client.AuthData;
 
 import com.firebase.client.Firebase;
+
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertNotNull;
 
-public class setRatingTest {
+public class setRatingTest extends InstrumentationTestCase {
     @Test
-    private Firebase fb = new Firebase(getResources().getString(R.string.firebase_url));
+    private Firebase fb = new Firebase(getInstrumentation().getContext().getResources().getString(R.string.firebase_url));
+    AuthData ad = fb.getAuth();
 
-    assertNull(fb.getAuth());
-    assertNotNull(fb.getAuth());
-
-
+    assertNull(ad);
+    assertNotNull(ad);
     );
 }
 
