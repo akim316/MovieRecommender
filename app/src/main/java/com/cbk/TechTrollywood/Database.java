47741d17eed68177f42dee8f44dac887792a402d
Mete0 <majitsu@gmail.com>
2016-04-11 21:45:14 -0400

    add studentmajor junit

--- app/src/main/java/com/cbk/TechTrollywood/Database.java
+++ app/src/main/java/com/cbk/TechTrollywood/Database.java
@@ -6,6 +6,7 @@ public abstract class Database {
     List recommendationsList = null;
 
     public abstract String getStudentMajorFor(String major);
+    public abstract Boolean setStudentMajor(String name, String major);
 
     public List getRecommendations(String searchMajor, String searchRating) {
         return recommendationsList;
--- app/src/test/java/com/cbk/movierecommender/UnitTestMajor.java
+++ app/src/test/java/com/cbk/movierecommender/UnitTestMajor.java
@@ -5,6 +5,7 @@ import com.cbk.TechTrollywood.Database;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -19,9 +20,13 @@ public class UnitTestMajor {
     public void testMajor(){
         Database db = mock(Database.class);
         when(db.getStudentMajorFor("Andy")).thenReturn("CS");
+        when(db.setStudentMajor("Andy", "CS")).thenReturn(true);
 
         assertEquals("CS", db.getStudentMajorFor("Andy"));
         assertNull(db.getStudentMajorFor("ASDF"));
+
+        assertEquals( true, (boolean) db.setStudentMajor("Andy","CS"));
+        assertFalse(db.setStudentMajor("ASDF", "CS"));
     }
 
 }
